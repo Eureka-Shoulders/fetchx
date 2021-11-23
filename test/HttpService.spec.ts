3bcc34495deb4ee905f12a7b76e88f3574301d86
Sampaio Leal <samp14hd@gmail.com>
2021-11-23 16:35:43 -0300

    chore: added httpservice tests

---
+++ test/HttpService.spec.ts
@@ -0,0 +1,99 @@
+import HttpService from '../src/HttpService';
+import mockServer from './mockedAPI';
+
+describe('HttpService', () => {
+  beforeAll(() => {
+    mockServer();
+  });
+
+  it('should be created', () => {
+    const httpService = new HttpService({ baseURL: '/api' });
+
+    expect(httpService).toBeTruthy();
+  });
+
+  it('should have the correct baseURL', () => {
+    const httpService = new HttpService({ baseURL: '/api' });
+
+    expect(httpService.client.defaults.baseURL).toBe('/api');
+  });
+
+  it('should have the correct default headers', () => {
+    const httpService = new HttpService({ baseURL: '/api' });
+
+    expect(httpService.client.defaults.headers.common).toEqual({
+      Accept: 'application/json, text/plain, */*',
+    });
+  });
+
+  it('should make a successfull GET request', async () => {
+    const httpService = new HttpService({ baseURL: '/api' });
+    const response = await httpService.client.get('/users');
+
+    expect(response.status).toBe(200);
+  });
+
+  it('should throw an error when the request fails', async () => {
+    const httpService = new HttpService({ baseURL: '/api' });
+
+    await expect(httpService.client.get('/does-not-exist')).rejects.toThrow();
+  });
+
+  it('should change headers with helper method', () => {
+    const httpService = new HttpService({ baseURL: '/api' });
+    const fakeToken = 'fake-token';
+
+    httpService.setHeader('authorization', fakeToken);
+
+    expect(httpService.client.defaults.headers.common).toHaveProperty(
+      'authorization',
+      fakeToken
+    );
+  });
+
+  it('should set request interceptor with helper method', async () => {
+    const httpService = new HttpService({ baseURL: '/api' });
+    const fakeToken = 'fake-token';
+
+    httpService.setRequestInterceptor('token-injector', config => {
+      if (config.headers) config.headers.authorization = fakeToken;
+
+      return config;
+    });
+
+    const response = await httpService.client.get('/users');
+
+    expect(response.config.headers).toHaveProperty('authorization', fakeToken);
+  });
+
+  it('should set response interceptor with helper method', async () => {
+    const httpService = new HttpService({ baseURL: '/api' });
+
+    httpService.setResponseInterceptor('now-injector', response => {
+      if (response.headers) response.headers.nowDate = new Date().toISOString();
+
+      return response;
+    });
+
+    const response = await httpService.client.get('/users');
+
+    expect(response.headers).toHaveProperty('nowDate');
+  });
+
+  it('should eject interceptors with helper method', async () => {
+    const httpService = new HttpService({ baseURL: '/api' });
+    const fakeToken = 'fake-token';
+
+    httpService.setRequestInterceptor('token-injector', config => {
+      if (config.headers) config.headers.authorization = fakeToken;
+
+      return config;
+    });
+
+    httpService.ejectInterceptor('token-injector', 'request');
+
+    const response = await httpService.client.get('/users');
+
+    expect(response.config.headers).not.toHaveProperty('authorization');
+  });
+});
