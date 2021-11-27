b466364c0b8dd601ebe076622fc3649a5c841cef
Sampaio Leal <samp14hd@gmail.com>
2021-11-27 13:21:53 -0300

    chore: removing useless comment

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,6 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import { terser } from 'rollup-plugin-terser';
-// import external from 'rollup-plugin-peer-deps-external';
 import postcss from 'rollup-plugin-postcss';
 import dts from 'rollup-plugin-dts';
 import excludeDependenciesFromBundle from 'rollup-plugin-exclude-dependencies-from-bundle';
