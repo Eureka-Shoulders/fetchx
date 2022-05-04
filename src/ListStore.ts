f56a645c6f8153cf323a4a00d40ea429a6efb229
Sampaio Leal <samp14hd@gmail.com>
2022-05-04 16:32:50 -0300

    fix: listore pagination params

--- src/ListStore.ts
+++ src/ListStore.ts
@@ -164,7 +164,7 @@ export default class ListStore<T = unknown> {
   }
 
   private setPaginationFilters() {
-    if (this.options.limit) {
+    if (this.options.limit !== undefined) {
       if (this.options.skipField) {
         this.filters.set(
           this.options.skipField,
